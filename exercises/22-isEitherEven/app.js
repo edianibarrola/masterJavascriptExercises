2fbefd4cd7527fac8b3a7cbd87ac124301c37a30
edianibarrola <edianibarrola@gmail.com>
2021-03-11 23:56:44 +0000

    saving

--- exercises/22-isEitherEven/app.js
+++ exercises/22-isEitherEven/app.js
@@ -1,1 +1,8 @@
 // Write your function here
+function isEitherEven(a,b){
+    if (a %2 == 0 || b%2 ==0){
+        return true;
+    }else return false;
+}
+var output = isEitherEven(1,4);
+console.log(output);
