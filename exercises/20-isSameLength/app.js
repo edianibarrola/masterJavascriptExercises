3363cf510e6a576d0b4d5aa1745e96311f15ca70
edianibarrola <edianibarrola@gmail.com>
2021-03-11 23:38:54 +0000

    saving

--- exercises/20-isSameLength/app.js
+++ exercises/20-isSameLength/app.js
@@ -1,6 +1,9 @@
 // Write your function here
-isSameLength = (word1, word2) => {
-    if (word1.length == word2.length)
+const isSameLength = (word1, word2) => {
+    if (word1.length == word2.length){
 
-
+        return true;
+    }else return false;
 }
+var output= isSameLength('words','super');
+console.log(output);
